41f9978cb238bc6f88f537fe49e470555c60bc37
adithyan_sukumar <72939103+gamenerd457@users.noreply.github.com>
2022-10-31 23:17:08 +0530

    Update imagenet_lsh.py

--- imagenet_lsh.py
+++ imagenet_lsh.py
@@ -376,7 +376,7 @@ def main_worker(gpu, ngpus_per_node, args, tb_logger, LOG):
     model_s.load_state_dict(avg_state_dict)
     test_acc, tect_acc_top5, test_loss = validate(val_loader, model_s, criterion, args)
     test_acc_test_set, tect_acc_top5_test_set , test_loss_test_set=test(test_loader,model_s, criterion, args)
-    print("{},{},{}".format(epoch, top1=test_acc_test_set, top5=tect_acc_top5_test_set))
+    print("{},{},{}".format(epoch, test_acc_test_set, tect_acc_top5_test_set))
     LOG.info(' * Avg 10 Acc@1 {top1:.3f} Acc@5 {top5:.3f}'.format(top1=test_acc, top5=tect_acc_top5))
     LOG.info(' * test set Avg 10 Acc@1  {top1:.3f} Acc@5 {top5:.3f}'
           .format(top1=test_acc_test_set, top5=tect_acc_top5_test_set))
